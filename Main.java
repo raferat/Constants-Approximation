5645704eca3712c6c29354d6b8477a7e11493679
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2021-09-29 20:01:38 +0200

    Patched viets

--- Main.java
+++ Main.java
@@ -1,68 +1,59 @@
 public class Main
 {
-    /**
-     *
-     * 1: 3.1415826535897198
-     */
-    private static final int count = 100_000;
+  private static final int count = 100_000;
+  
+  public static void main(String[] args) 
+  {
     
-    public static void main(String[] args) 
+    System.out.println ( "Gregoryho  : " + gregoryho_leibnitzova_rada () );
+    System.out.println ( "Wallis     : " + john_wallis() );
+    System.out.println ( "Viets      : " + viets () );
+    System.out.println ( "Archimedes : " + arch ( 0 , 2*Math.sqrt(3) , 3 ) );
+    System.out.println ( "Math.PI    : " + Math.PI );
+  }
+  
+  private static final double gregoryho_leibnitzova_rada ()
+  {
+    double result = 0;
+    for ( int i = 0 ; i < count ; i ++ )
     {
-      
-      System.out.println ( "Gregoryho  : " + gregoryho_leibnitzova_rada () );
-      System.out.println ( "Wallis     : " + john_wallis() );
-      System.out.println ( "Viets      : " + viets () );
-      System.out.println ( "Archimedes : " + arch ( 0 , 2*Math.sqrt(3) , 3 ) );
-      System.out.println ( "Math.PI    : " + Math.PI );
+        result += (Math.pow(-1,i)/(2.0*i+1));
     }
-    
-    private static final double gregoryho_leibnitzova_rada ()
+    result *= 4;
+    return result;
+  }
+  
+  private static final double john_wallis ()
+  {
+    double result = 1;
+    for ( int i = 1 ; i < count ; i ++ )
     {
-      double result = 0;
-      for ( int i = 0 ; i < count ; i ++ )
-      {
-          result += (Math.pow(-1,i)/(2.0*i+1));
-      }
-      result *= 4;
-      return result;
+      result *= ((Math.pow(2*i,2)) / (Math.pow ( 2 * i , 2 ) - 1));
     }
-
-    private static final double john_wallis ()
+    return result*2;
+  }
+  
+  private static final double viets ()
+  {
+    double sqrts = 0;
+    double result = 1;
+    for ( int i = 0 ; i < count ; i ++ )
     {
-      double result = 1;
-      for ( int i = 1 ; i < count ; i ++ )
-      {
-        result *= ((Math.pow(2*i,2)) / (Math.pow ( 2 * i , 2 ) - 1));
-      }
-      return result*2;
+	     sqrts = Math.sqrt ( 2.0 + sqrts );
+      result *= sqrts / 2;
     }
-
-    private static final double viets ()
+    return 2.0 * ( 1.0 / result );
+  }
+  
+  private static final double arch ( int count , double last_a , double last_b )
+  {
+    if ( count < 8_000 )
     {
-      double sqrts = 0;
-      for ( int i = 0 ; i < 20 ; i ++ )
-      {
-//System.out.println ( sqrts );
-	sqrts = Math.sqrt ( 2.0 + sqrts );
-
-
-      }
-      sqrts /= Math.pow ( 2 , 20 );
-      return 2.0 * (1.0/sqrts);
+	      double a = ( 2 * last_a * last_b ) / ( last_a + last_b );
+	      double b = Math.sqrt ( a * last_b );
+	      return arch ( count +1 , a , b );
     }
-
-    private static final double arch ( int count , double last_a , double last_b )
-    {
-      if ( count < 2000 )
-      {
-	double a = ( 2 * last_a * last_b ) / ( last_a + last_b );
-	double b = Math.sqrt ( a * last_b );
-	return arch ( count +1 , a , b );
-      }
-      else 
-	return last_b;
-    }
-
-
-    
+    else 
+	     return last_b;
+  }
 }
