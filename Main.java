cb7f80049f5ff2b160c8752c6f4101241bcedfc1
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2021-09-29 20:13:12 +0200

    Changed alignment.

--- Main.java
+++ Main.java
@@ -1,59 +1,59 @@
 public class Main
 {
-  private static final int count = 100_000;
-  
-  public static void main(String[] args) 
+private static final int exactness = 100_000;
+private static final int antiOwerflowExactness = 8_000;
+
+public static void main(String[] args) 
+{
+  System.out.println ( "Gregoryho  : " + gregoryhoLeibnitzovaRada () );
+  System.out.println ( "Wallis     : " + johnWallis() );
+  System.out.println ( "Viets      : " + viets () );
+  System.out.println ( "Archimedes : " + arch ( 0 , 2*Math.sqrt(3) , 3 ) );
+  System.out.println ( "Math.PI    : " + Math.PI );
+}
+
+private static final double gregoryhoLeibnitzovaRada ()
+{
+  double result = 0;
+  for ( int i = 0 ; i < exactness ; i ++ )
   {
-    
-    System.out.println ( "Gregoryho  : " + gregoryho_leibnitzova_rada () );
-    System.out.println ( "Wallis     : " + john_wallis() );
-    System.out.println ( "Viets      : " + viets () );
-    System.out.println ( "Archimedes : " + arch ( 0 , 2*Math.sqrt(3) , 3 ) );
-    System.out.println ( "Math.PI    : " + Math.PI );
+    result += (Math.pow(-1,i)/(2.0*i+1));
   }
-  
-  private static final double gregoryho_leibnitzova_rada ()
+  result *= 4;
+  return result;
+}
+
+private static final double johnWallis ()
+{
+  double result = 1;
+  for ( int i = 1 ; i < exactness ; i ++ )
   {
-    double result = 0;
-    for ( int i = 0 ; i < count ; i ++ )
-    {
-        result += (Math.pow(-1,i)/(2.0*i+1));
-    }
-    result *= 4;
-    return result;
+    result *= ((Math.pow(2*i,2)) / (Math.pow ( 2 * i , 2 ) - 1));
   }
-  
-  private static final double john_wallis ()
+  return result*2;
+}
+
+private static final double viets ()
+{
+  double sqrts = 0;
+  double result = 1;
+  for ( int i = 0 ; i < exactness ; i ++ )
   {
-    double result = 1;
-    for ( int i = 1 ; i < count ; i ++ )
-    {
-      result *= ((Math.pow(2*i,2)) / (Math.pow ( 2 * i , 2 ) - 1));
-    }
-    return result*2;
+    sqrts = Math.sqrt ( 2.0 + sqrts );
+    result *= sqrts / 2;
   }
-  
-  private static final double viets ()
+  return 2.0 * ( 1.0 / result );
+}
+
+private static final double arch ( int exactness , double lastA , double lastB )
+{
+  if ( exactness < antiOwerflowExactness )
   {
-    double sqrts = 0;
-    double result = 1;
-    for ( int i = 0 ; i < count ; i ++ )
-    {
-	     sqrts = Math.sqrt ( 2.0 + sqrts );
-      result *= sqrts / 2;
-    }
-    return 2.0 * ( 1.0 / result );
+    double a = ( 2 * lastA * lastB ) / ( lastA + lastB );
+    double b = Math.sqrt ( a * lastB );
+    return arch ( exactness +1 , a , b );
   }
-  
-  private static final double arch ( int count , double last_a , double last_b )
-  {
-    if ( count < 8_000 )
-    {
-	      double a = ( 2 * last_a * last_b ) / ( last_a + last_b );
-	      double b = Math.sqrt ( a * last_b );
-	      return arch ( count +1 , a , b );
-    }
-    else 
-	     return last_b;
+  else 
+    return lastB;
   }
 }
